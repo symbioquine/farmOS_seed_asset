6b8b58f735243ef72a629abef48e7e6a80b1de07
Symbioquine <symbioquine@gmail.com>
2021-02-14 14:45:01 -0800

    Minimal seed asset implementation - no extra fields or migration yet

---
+++ farmos_seed_asset/src/Plugin/Asset/AssetType/Seed.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\farmos_seed_asset\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the seed asset type.
+ *
+ * @AssetType(
+ *   id = "seed",
+ *   label = @Translation("Seed"),
+ * )
+ */
+class Seed extends FarmAssetType {
+
+}
+

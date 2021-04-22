db9484bc643c409ede390a5c02671f82f666c6b3
Symbioquine <symbioquine@gmail.com>
2021-04-22 09:40:58 -0700

    Add crop/variety and season fields to seed asset

--- farmos_seed_asset/src/Plugin/Asset/AssetType/Seed.php
+++ farmos_seed_asset/src/Plugin/Asset/AssetType/Seed.php
@@ -14,5 +14,45 @@ use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
  */
 class Seed extends FarmAssetType {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+    $field_info = [
+      'plant_type' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Crop/variety'),
+        'description' => "Enter this plant asset's crop/variety.",
+        'target_type' => 'taxonomy_term',
+        'target_bundle' => 'plant_type',
+        'auto_create' => TRUE,
+        'required' => TRUE,
+        'multiple' => TRUE,
+        'weight' => [
+          'form' => -90,
+          'view' => -90,
+        ],
+      ],
+      'season' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Season'),
+        'description' => $this->t('Assign this to a season for easier searching later.'),
+        'target_type' => 'taxonomy_term',
+        'target_bundle' => 'season',
+        'auto_create' => TRUE,
+        'multiple' => TRUE,
+        'weight' => [
+          'form' => -50,
+          'view' => -50,
+        ],
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $this->farmFieldFactory->bundleFieldDefinition($info);
+    }
+    return $fields;
+  }
+
 }
 
